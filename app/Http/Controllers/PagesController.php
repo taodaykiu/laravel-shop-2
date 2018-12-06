d5a667fceb415136eb0de63573b066cb5c6367b3
vagrant <vagrant@homestead>
2018-12-06 18:59:57 +0000

    邮箱验证中间件

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PageController extends Controller
+class PagesController extends Controller
 {
     //
     public function root()
     {
         return view('pages.root');
+    }
+
+    public function emailVerifyNotice()
+    {
+        return view('pages.email_verify_notice');
     }
 }
