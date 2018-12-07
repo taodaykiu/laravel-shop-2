68c4c502a5dab81c8cbf5a4e974e0d8a1ce6924e
dahai <491753317@qq.com>
2018-12-07 23:40:35 +0800

    触发激活邮件

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,17 @@
 |
 */
 
-Route::get('/','PagesController@root')->name('root');
+Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
 
-Route::group(['middleware' => 'auth'],function(){
+Route::group(['middleware' => 'auth'], function () {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     // 开始
-    Route::group(['middleware' => 'email_verified'], function() {
-        Route::get('/test', function() {
-            return 'Your email is verified';
-        });
+    Route::group(['middleware' => 'email_verified'], function () {
+
     });
     // 结束
 });
