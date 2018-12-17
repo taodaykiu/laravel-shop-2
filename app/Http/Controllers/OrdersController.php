652e1b26640c0466e58c3d9aa7184e19958a2925
dahai <491753317@qq.com>
2018-12-17 15:07:54 +0800

    用户界面 - 申请退款

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,17 +4,60 @@ namespace App\Http\Controllers;
 
 use App\Events\OrderReviewed;
 use App\Exceptions\InvalidRequestException;
+use App\Http\Requests\ApplyRefundRequest;
 use App\Http\Requests\OrderRequest;
-use App\Models\UserAddress;
-use App\Models\Order;
-use App\Services\OrderService;
-use Illuminate\Http\Request;
-use Carbon\Carbon;
 use App\Http\Requests\SendReviewRequest;
+use App\Models\Order;
+use App\Models\UserAddress;
+use App\Services\OrderService;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class OrdersController extends Controller
 {
     // 利用 Laravel 的自动解析功能注入 CartService 类
+    public function index(Request $request)
+    {
+        $orders = Order::query()
+            // 使用 with 方法预加载，避免N + 1问题
+                       ->with(['items.product', 'items.productSku'])
+                       ->where('user_id', $request->user()->id)
+                       ->orderBy('created_at', 'desc')
+                       ->paginate();
+
+        return view('orders.index', compact('orders'));
+    }
+
+    public function show(Order $order, Request $request)
+    {
+        $this->authorize('own', $order);
+
+        // load 延迟预加载 通常在返回一条 Order 使用
+        return view('orders.show', ['order' => $order->load(['items.product', 'items.productSku'])]);
+    }
+
+    public function received(Order $order, Request $request)
+    {
+        // 校验权限
+        $this->authorize('own', $order);
+
+        // 判断订单的发货状态是否为已发货
+        if ($order->ship_status !== Order::SHIP_STATUS_DELIVERED) {
+            throw new InvalidRequestException('发货状态不正确');
+        }
+
+        // 更新发货状态为已收到
+        $order->update(['ship_status' => Order::SHIP_STATUS_RECEIVED]);
+
+        if ($request->expectsJson()) {
+            // 返回订单信息
+            return $order;
+        }
+
+        // 返回原页面
+        return redirect()->back();
+    }
+
     public function store(OrderRequest $request, OrderService $orderService)
     {
         $user = $request->user();
@@ -23,54 +66,15 @@ class OrdersController extends Controller
         return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
     }
 
-    public function index(Request $request)
-    {
-        $orders = Order::query()
-                        // 使用 with 方法预加载，避免N + 1问题
-                       ->with(['items.product', 'items.productSku'])
-                       ->where('user_id', $request->user()->id)
-                       ->orderBy('created_at', 'desc')
-                       ->paginate();
-
-        return view('orders.index',compact('orders'));
-    }
-
-    public function show(Order $order,Request $request)
-    {
-        $this->authorize('own', $order);
-        // load 延迟预加载 通常在返回一条 Order 使用
-        return view('orders.show',['order' =>$order->load(['items.product','items.productSku'])]);
-    }
-
-    public function received(Order $order, Request $request)
-    {
-        // 校验权限
-        $this->authorize('own',$order);
-
-        // 判断订单的发货状态是否为已发货
-        if ($order->ship_status !== Order::SHIP_STATUS_DELIVERED){
-            throw new InvalidRequestException('发货状态不正确');
-        }
-
-        // 更新发货状态为已收到
-        $order->update(['ship_status' => Order::SHIP_STATUS_RECEIVED]);
-
-        if ($request->expectsJson()){
-            // 返回订单信息
-            return $order;
-        }
-        // 返回原页面
-        return redirect()->back();
-    }
-
     public function review(Order $order)
     {
         // 校验权限
         $this->authorize('own', $order);
         // 判断是否已经支付
-        if (!$order->paid_at) {
+        if ( !$order->paid_at) {
             throw new InvalidRequestException('该订单未支付，不可评价');
         }
+
         // 使用 load 方法加载关联数据，避免 N + 1 性能问题
         return view('orders.review', ['order' => $order->load(['items.productSku', 'items.product'])]);
     }
@@ -79,7 +83,7 @@ class OrdersController extends Controller
     {
         // 校验权限
         $this->authorize('own', $order);
-        if (!$order->paid_at) {
+        if ( !$order->paid_at) {
             throw new InvalidRequestException('该订单未支付，不可评价');
         }
         // 判断是否已经评价
@@ -105,5 +109,30 @@ class OrdersController extends Controller
         });
 
         return redirect()->back();
+    }
+
+    public function applyRefund(Order $order, ApplyRefundRequest $request)
+    {
+        // 校验订单是否属于当前用户
+        $this->authorize('own', $order);
+
+        // 判断订单是否已付款
+        if ( !$order->paid_at) {
+            throw new InvalidRequestException('该订单未支付，不能申请退款');
+        }
+        // 判断订单退款状态是否正确
+        if ($order->refund_status !== Order::REFUND_STATUS_PENDING) {
+            throw new InvalidRequestException('该订单已经申请过退款，请勿重复申请');
+        }
+        // 将用户输入的退款理由放到订单的 extra 字段中
+        $extra = $order->extra ?: [];
+        $extra['refund_reason'] = $request->input('reason');
+        // 将订单退款状态改为已申请退款
+        $order->update([
+                           'refund_status' => Order::REFUND_STATUS_APPLIED,
+                           'extra'         => $extra,
+                       ]);
+
+        return $order;
     }
 }
