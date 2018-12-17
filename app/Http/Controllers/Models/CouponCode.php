7111296a59e1d32e00ddfd66390011a8b73c4888
dahai <491753317@qq.com>
2018-12-18 00:18:47 +0800

    管理后台 - 优惠券列表

---
+++ app/Http/Controllers/Models/CouponCode.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Models;
+
+use App\f;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CouponCode extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\f  $f
+     * @return \Illuminate\Http\Response
+     */
+    public function show(f $f)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\f  $f
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(f $f)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\f  $f
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, f $f)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\f  $f
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(f $f)
+    {
+        //
+    }
+}
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -101,4 +101,9 @@ class Order extends Model
 
         return $no;
     }
+
+    public function couponCode()
+    {
+        return $this->belongsTo(CouponCode::class);
+    }
 }
