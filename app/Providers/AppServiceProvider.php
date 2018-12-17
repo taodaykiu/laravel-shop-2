b329045a4d84912c0750cb33b484b708bc35125a
dahai <491753317@qq.com>
2018-12-17 17:26:39 +0800

    后台管理 - 同意退款 - 支付宝退款

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,7 +34,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton('alipay', function () {
             $config = config('pay.alipay');
             //$config['notify_url'] = route('payment.alipay.notify');
-            $config['notify_url'] = 'http://requestbin.fullcontact.com/qzfqz3qz';
+            $config['notify_url'] = 'http://requestbin.fullcontact.com/vz0dzovz';
             $config['return_url'] = route('payment.alipay.return');
             // 判断当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
