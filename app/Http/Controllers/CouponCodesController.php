842cac04a1a949b911ab38c151840a3df66e0788
dahai <491753317@qq.com>
2018-12-18 20:37:06 +0800

    优化优惠券模块

--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -3,20 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\CouponCode;
-use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class CouponCodesController extends Controller
 {
     //
-    public function show($code)
+    public function show($code, Request $request)
     {
         //abort() 方法可以直接中断我们程序的运行，接受的参数会变成 Http 状态码返回
         // 判断优惠券是否存在
-        if (!$record = CouponCode::query()->where('code', $code)->first()) {
+        if ( !$record = CouponCode::query()->where('code', $code)->first()) {
             abort(404);
         }
 
-        $record->checkAvailable();
+        $record->checkAvailable($request->user());
 
         return $code;
     }
